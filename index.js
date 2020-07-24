ace382b949d4e9a98334104ebaeda831ffe06ae1
fvalencia <fvalencia@localhost.localdomain>
2020-07-24 11:22:19 -0500

    update de contrato

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ const getExceptions = require('./Json/Monitor/exceptions/getExeptions.json')
 const getPeriodicity = require('./Json/Periodicity.json')
 const getPeriod = require('./Json/Period.json')
 const getCellById = require('./Json/MassDownload/GetCellById.json')
+const getFilterQuery = require('./Json/Query.json')
 
 const saveInvoiceError = require('./Json/SaveInvoiceError.json')
 const getPdf = require('./Json/Vouchers/BillingNotice/GetPdf.json')
@@ -495,6 +496,9 @@ app.get('/gestion/configurations/personType', (req, res) => {
 })
 app.get('/gestion/configurations/nationality', (req, res) => {
   res.status(200).send(onlineInvoice.nationality)
+})
+app.get('/getFilterQuery', (req, res) => {
+  res.status(200).send(getFilterQuery)
 })
 
 app.listen(8080, () => {
